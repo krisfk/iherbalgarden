53ba893f122839b7624ecfa892a776eaba867516
kaiyu li <krisfk@gmail.com>
2022-07-15 14:38:13 +0800

    .

--- wp-content/themes/iherbalgarden/page-school-intro.php
+++ wp-content/themes/iherbalgarden/page-school-intro.php
@@ -222,7 +222,7 @@ if($school_logo_url)
                 </div> -->
                     <div class="col-lg-6 col-md-12 col-sm-12 col-12 mb-4">
 
-                        <table>
+                        <table style="display:none;">
                             <tr>
                                 <td>
                                     <div class="">
